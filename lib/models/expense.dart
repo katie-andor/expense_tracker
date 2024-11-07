ccbadbb347f6149d6c3ff78c99fee2544607ff4c
Katie Andor <superkga5678@gmail.com>
2024-11-07 11:54:35 -0500

    fixed date format and category icons

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,7 +1,19 @@
+import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
+import 'package:intl/intl.dart';
+
 const uuid = Uuid();
 
-enum Category {food, travel, lesiure, work}
+enum Category {food, travel, leisure, work}
+
+final formatter = DateFormat.yMd();
+
+const categoryIcons = {
+  Category.food: Icons.lunch_dining,
+  Category.travel: Icons.flight_takeoff,
+  Category.leisure: Icons.movie,
+  Category.work: Icons.work
+};
 
 class Expense {
   Expense({required this.title, required this.amount, required this.date, required this.category})
@@ -11,4 +23,8 @@ class Expense {
   final double amount;
   final DateTime date;
   final Category category; 
+
+  String get formattedDate{
+    return formatter.format(date);
+  }
 }
